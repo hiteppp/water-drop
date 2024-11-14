5498844ec0b96ca12a95fcb5396e44f522e368e1
hiteppp <2316674694@qq.com>
2024-11-14 08:38:49 +0800

    feat:apollo封装

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import { useEffect, useState } from 'react';
-
 import './App.css';
-import useLatest from './hooks/useLatest';
+import { useQuery } from '@apollo/client';
+import { FIND } from './graphql/demo';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const latestCount = useLatest(count);
+  const { loading, data } = useQuery(FIND, {
+    variables: {
+      id: 1,
+    },
+  });
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      //console.log(latestCount.current); // 这里总是打印最新的 count 值
-      console.log('count的值是', count);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return <button onClick={() => setCount(count + 1)}>Increment</button>;
+  return (
+    <div>
+      <p>data:{JSON.stringify(data)}</p>
+      <p>data:{loading}</p>
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import { ApolloProvider } from '@apollo/client';
+import './index.css';
+import App from './App.tsx';
+import { client } from './utils/apollo.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
-)
+  </ApolloProvider>
+);
